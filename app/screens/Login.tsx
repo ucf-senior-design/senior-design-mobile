52c476434971d5f45c76cb12b5dcce9e7cff02a7
Victoria Williamson <vicwill01@knights.ucf.edu>
2022-12-17 17:59:09 -0500

    added basic validation to login page

--- app/screens/Login.tsx
+++ app/screens/Login.tsx
@@ -1,13 +1,20 @@
 import { observer } from "mobx-react-lite"
-import React, { FC } from "react"
-import { ImageStyle, Image, View, ViewStyle } from "react-native"
-import { Header, Screen, Text, TextField, Button } from "../components"
-import { AppStackScreenProps, navigate } from "../navigators"
-import { colors, spacing, typography } from "../theme"
-
+import React, { FC, useState } from "react"
+import { View, ViewStyle } from "react-native"
+import { Screen, Text, TextField, Button, Icon } from "../components"
+import { AppStackScreenProps } from "../navigators"
+import { spacing } from "../theme"
+import * as EmailValidator from "email-validator"
 interface LoginProps extends AppStackScreenProps<"Login"> {}
 
 export const Login: FC<LoginProps> = observer(function LoginScreen({ navigation }) {
+  const [errorMessage, sErrorMessage] = useState("")
+  const [user, sUser] = useState({
+    email: "",
+    password: "",
+  })
+
+  const isEmailInvalid = user.email.length === 0 || !EmailValidator.validate(user.email)
   return (
     <>
       <Screen
@@ -23,12 +30,31 @@ export const Login: FC<LoginProps> = observer(function LoginScreen({ navigation 
             text="log in to continue managing your trips!"
             style={{ textAlign: "center" }}
           />
-          <TextField label="email" placeholder="email@domain.com" />
+          <TextField
+            status={isEmailInvalid ? "error" : undefined}
+            helper={isEmailInvalid ? "invalid email" : undefined}
+            inputWrapperStyle={isEmailInvalid && { marginBottom: 0 }}
+            containerStyle={isEmailInvalid && { marginVertical: spacing.extraSmall }}
+            label="email"
+            placeholder="email@domain.com"
+            onChangeText={(e) =>
+              sUser((user) => ({
+                ...user,
+                email: e,
+              }))
+            }
+          />
 
           <TextField
             label="password"
             placeholder="password"
             canBeHidden={true}
+            onChangeText={(e) =>
+              sUser((user) => ({
+                ...user,
+                password: e,
+              }))
+            }
             inputWrapperStyle={{ marginBottom: 0 }}
           />
           <View
@@ -40,10 +66,10 @@ export const Login: FC<LoginProps> = observer(function LoginScreen({ navigation 
               preset="noFill"
               text="forgot password?"
               textStyle={{ fontSize: 10 }}
-              style={{ justifyContent: "flex-end", margin: 0}}
+              style={{ justifyContent: "flex-end", margin: 0 }}
             />
           </View>
-          <Button text="log in"/>
+          <Button text="log in" />
         </View>
       </Screen>
     </>
