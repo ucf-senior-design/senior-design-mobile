fcceb343faa243c2236a2328494eb725d5c71f89
aamare1001 <aamare100@gmail.com>
2023-03-05 20:23:48 -0500

    Setting Template for Account Page

--- app/screens/Account.tsx
+++ app/screens/Account.tsx
@@ -4,22 +4,29 @@ import { observer } from "mobx-react-lite"
 import { Button, Icon, Screen, Text } from "../components"
 import { AppStackScreenProps } from "../navigators"
 import { useAuth } from "../models/hooks/authentication"
+import { Avatar } from "@ui-kitten/components"
 
 type AccountProps = AppStackScreenProps<"Account">
 export const Account: FC<AccountProps> = observer(function ViewTripScreen() {
   const { doLogout } = useAuth()
   return (
     <Screen>
-      <View style={{ flex: 1 }}>
-        <Icon
-          icon="personSmall"
-          style={{
-            width: "100%",
-          }}
-        />
-        <Text text="Account screen" />
-        <Button text="logout" onPress={() => doLogout()} />
+      <View style={{paddingHorizontal: 20 , flexDirection: "row"}}>
+        <Avatar 
+        style={{borderRadius: 100, minWidth: "30%", minHeight: 100}}
+        source={{uri: "https://akveo.github.io/react-native-ui-kitten/docs/assets/playground-build/static/media/icon.a78e4b51.png"}} />
+        <View style={{paddingLeft: 30}}>
+          <Text text="Ima Knight" preset="bold" size="xl"/>
+          <Text text="email@domain.com" preset="formLabel"/>
+        </View>
       </View>
+      <View>
+        <Text text="Profile" preset="heading" size="xl"/>
+      </View>
+      <View>
+        <Text text="Notifications" preset="heading" size="xl"/>
+      </View>
+        <Button text="logout" onPress={() => doLogout()} />
     </Screen>
   )
 })
